566183c65b49269d76a2c4d5d879fff1662b8ee8
Mihkel Rebane <mihkel.fox@gmail.com>
2023-11-10 22:35:42 +0200

    Task 3
    Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/index.js
+++ src/index.js
@@ -23,24 +23,23 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {
+          Array(3).fill(null).map((_, i) => (
+            <div key={i} className="board-row">
+              {
+                Array(3).fill(null).map((_, j) => (
+                  <span key={3 * i + j}>
+                    {this.renderSquare(3 * i + j)}
+                  </span>
+                ))
+              }
+            </div>
+          ))
+        }
       </div>
     );
   }
+  
 }
 
 class Game extends React.Component {
